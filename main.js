f9014963b2363e822be0f23d1a267bc0b35ed608
1977Suryaa <101641242+1977Suryaa@users.noreply.github.com>
2022-09-30 16:10:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ function setup(){
 canvas=createCanvas(400,400)
 webcam=createCapture(VIDEO)
 webcam.hide()
+    filter1=""
 }
 function draw(){
 image(webcam,0,0,400,400)
 tint(filter1)
 }
-function filter(){
+function filter3(){
     filter1=document.getElementById("input").value
 
 
